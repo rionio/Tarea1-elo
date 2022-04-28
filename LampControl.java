f24d64cea6a1e94a1dbd9399fe4787f1d7bedfa0
diegomaldonadom <diego.maldonadom@usm.cl>
2022-04-27 22:27:30 -0400

    wea2

--- LampControl.java
+++ LampControl.java
@@ -2,15 +2,20 @@ import javafx.collections.ListChangeListener.Change;
 
 public class LampControl {
     public LampControl(int channel, Cloud c){
-        //???
+        this.channel = channel;
+        cloud = c;
     }
     public void pressPower(){
         cloud.changeLampPowerState(channel);
     }
     public int getChannel(){
-        //????
+        return channel;
     }
-    //???sdfsds
+    public void up_red(){
+        cloud.up_red
+    }
+
+
     private Cloud cloud;
     private int channel;
 }
